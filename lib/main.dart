3a7d0a50ace1dc535a89d38970ce906c2b12a606
Mohammedquraysh <mohammedquraysh4@gmail.com>
2026-09-10 11:18:25 +0100

    Added unknown my files

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+   title: 'Mohammed New App',
+    home: Scaffold(
+      appBar:  AppBar(
+         title: Text('New App'),
+        centerTitle: true,
+        elevation: 20.0,
+        backgroundColor: Colors.blue[900],
+      ),
+    ),
+  ));
+}
+
+
