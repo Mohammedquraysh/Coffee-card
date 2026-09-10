bf6d62681457ea08e0f8754e654b3312419e0035
Mohammedquraysh <mohammedquraysh4@gmail.com>
2026-09-10 11:28:05 +0100

    added build for stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,17 @@ void main() {
         elevation: 20.0,
         backgroundColor: Colors.blue[900],
       ),
+      body: Home(),
     ),
   ));
 }
 
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hello ');
+  }
+}
 
